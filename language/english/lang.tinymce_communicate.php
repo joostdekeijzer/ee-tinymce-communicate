8fc6ef82452395224b0d0a124fc6a92ba189a794
joost <macjoost@gmail.com>
2010-03-05 17:38:31 +0100

    Extending tinymce_communicate to work with other TinyMCE implementations  * Directory structure  * Lang file  * Added settings

---
+++ language/english/lang.tinymce_communicate.php
@@ -0,0 +1,24 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"Use_LG_TinyMCE" =>
+"Use LG TinyMce",
+
+"TinyMCE_Script_URL" =>
+"TinyMCE Script URL",
+
+"TinyMCE_Config_URL" =>
+"TinyMCE Config URL",
+
+"TinyMCE_Class_Selector" =>
+"TinyMCE Class Selector",
+
+/* END */
+''=>''
+);
+?>
